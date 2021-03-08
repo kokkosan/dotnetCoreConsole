cffb5dacaaae077bcb7566b150197ddb8215d4aa
kokkosan <yusukeiguchi116621@gmail.com>
2021-03-08 16:07:46 +0900

    Console App init .

---
+++ SampleAppCoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleAppCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
